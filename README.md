f4781c3b6c3080d9b42bb229891576f55c3d4052
Florian Oellerich <f.oellerich@epages.com>
2015-12-09 13:25:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# scribe-plugin-paragraph-command
+Command to change a text block in [scribe](https://github.com/guardian/scribe/) into a paragraph.
+Without this plugin you can only change to paragraph by deactivating any of the other block commands.
+
+## Usage
+```javascript
+var paragraphCommand = require('scribe-plugin-paragraph-command');
+scribe.use(paragraphCommand());
+```
+
+## Installation
+```
+bower install scribe-plugin-paragraph-command
+```
---
+++ src/scribe-plugin-paragraph-command.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+
+    var scribePluginParagraphCommand = function () {
+        return function (scribe) {
+            var tag = '<p>';
+            var nodeName = 'P';
+            var commandName = 'p';
+            var paragraphCommand = new scribe.api.Command('formatBlock');
+
+            paragraphCommand.execute = function () {
+                scribe.api.Command.prototype.execute.call(this, tag);
+            };
+
+            paragraphCommand.queryState = function () {
+                var selection = new scribe.api.Selection();
+                return selection.getContaining(function (node) {
+                    return node.nodeName === nodeName;
+                });
+            };
+
+            paragraphCommand.queryEnabled = function () {
+                var selection = new scribe.api.Selection();
+                var listNode = selection.getContaining(function (node) {
+                    return node.nodeName === 'OL' || node.nodeName === 'UL';
+                });
+
+                return scribe.api.Command.prototype.queryEnabled.apply(this, arguments) && scribe.allowsBlockElements() && !listNode;
+            };
+
+            scribe.commands[commandName] = paragraphCommand;
+        };
+    };
+
+    // Module system magic dance
+    if (typeof module !== 'undefined') {
+        module.exports = scribePluginParagraphCommand;
+    } else if (typeof define === 'function' && typeof define.amd === 'object') {
+        define(function () {
+            return scribePluginParagraphCommand;
+        });
+    } else {
+        window.scribePluginParagraphCommand = scribePluginParagraphCommand;
+    }
+}());
